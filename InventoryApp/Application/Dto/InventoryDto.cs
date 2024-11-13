a467adbd328d5ff9598907ed0233f2f91513a4c0
cemgx <c-e-m-g-o-l@hotmail.com>
2024-11-13 17:34:08 +0300

    starting InventoryController

--- InventoryApp/Application/Dto/InventoryDto.cs
+++ InventoryApp/Application/Dto/InventoryDto.cs
@@ -6,8 +6,11 @@ namespace InventoryApp.Application.Dto
     {
         public int Id { get; set; }
         public int GivenByEmployeeId { get; set; }
+        public string? GivenByEmployeeName { get; set; }
         public int ReceivedByEmployeeId { get; set; }
+        public string? ReceivedByEmployeeName { get; set; }
         public int ProductId { get; set; }
+        public string? ProductName { get; set; }
         public DateTime? DeliveredDate { get; set; }
         public DateTime? ReturnDate { get; set; }
     }
--- InventoryApp/Controllers/InventoryController.cs
+++ InventoryApp/Controllers/InventoryController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using InventoryApp.Application.Dto;
+using InventoryApp.Application.Interfaces;
+using InventoryApp.Models.Entity;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InventoryApp.Controllers
@@ -7,5 +11,28 @@ namespace InventoryApp.Controllers
     [ApiController]
     public class InventoryController : ControllerBase
     {
+        private readonly IRepository<Inventory> repository;
+        private readonly IMapper mapper;
+
+        public InventoryController(IRepository<Inventory> repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateInventory([FromBody] InventoryDto inventoryDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var inventory = mapper.Map<Inventory>(inventoryDto);
+
+            await repository.CreateAsync(inventory);
+
+            var createdInventoryDto = mapper.Map<InventoryDto>(inventory);
+            return Created("", createdInventoryDto);
+        }
+
     }
 }
